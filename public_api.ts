2797d90ca7218cde8a0d6ebf318b47f206d79095
giovanni.sorgente <giovanni.sorgente@GIOVANNI-PC>
2018-01-30 08:44:58 +0100

    Angular 5
    Upgraded refs to Angular5, added ng-packagr as dev ref and created release build for npm

---
+++ public_api.ts
@@ -0,0 +1,7 @@
+export * from './src/app/ng4-files/ng4-files.module';
+// export * from './src/app/ng4-files/services';
+// export * from './src/app/ng4-files/declarations';
+export * from './src/app/ng4-files/services/ng4-files.service';
+export * from './src/app/ng4-files/services/ng4-files-utils.service';
+export * from './src/app/ng4-files/declarations/ng4-files-config';
+export * from './src/app/ng4-files/declarations/ng4-files-selected';
